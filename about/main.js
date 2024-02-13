6dc6840081f7583a6a8f62b244e7771b768c0520
erica <ericakimhay03@gmail.com>
2024-02-13 10:36:42 -0600

    bugs

--- about/main.js
+++ about/main.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function() {
 var projects = document.querySelectorAll('.project');
 projects.forEach(function(project, index) {
     project.addEventListener('click', function() {
-        var urls = ['https://github.com/ericaahayy/ZooWebApp', 'ff.html', 'https://github.com/ericaahayy/ITGurlsCOSC3337/tree/main', 'https://github.com/ericaahayy/WineQuality'];
+        var urls = ['https://github.com/ericaahayy/ZooWebApp', 'https://github.com/niftybif/FrogGame', 'https://github.com/ericaahayy/ITGurlsCOSC3337/tree/main', 'https://github.com/ericaahayy/WineQuality'];
         window.location.href = urls[index];
     });
 });
